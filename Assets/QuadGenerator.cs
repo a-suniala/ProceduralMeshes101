8bece023fc638bf9a5f81b6246b1a3e2ffc79592
Sunave <antti.suniala@gmail.com>
2015-08-27 09:15:56 +0300

    Initial commit, add QuadGenerator

---
+++ Assets/QuadGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(MeshFilter))]
+public class QuadGenerator : MonoBehaviour {
+
+	void Start () {
+		Vector3[] vertices = new Vector3[4];
+
+		vertices[0] = new Vector3(0,0,0);
+		vertices[1] = new Vector3(0,1,0);
+		vertices[2] = new Vector3(1,1,0);
+		vertices[3] = new Vector3(1,0,0);
+
+		int[] triangles = new int[6];
+		triangles[0] = 0;
+		triangles[1] = 1;
+		triangles[2] = 2;
+		triangles[3] = 0;
+		triangles[4] = 2;
+		triangles[5] = 3;
+
+		Mesh mesh = new Mesh();
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.RecalculateBounds();
+
+		MeshFilter mf = GetComponent<MeshFilter>();
+		mf.mesh = mesh;
+	}
+
+}
